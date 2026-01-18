c4251213c87e1242c93b3afea5747fa1093065ba
raiankit0125 <raiankit0125@gmail.com>
2026-01-18 23:55:24 +0530

    CORS env based

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,11 @@ import bulkRoutes from "./routes/bulk.routes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
